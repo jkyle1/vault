2b4fe40d0021aee69a450858acd1b1ef32cc0b87
kyle.smallwood@gmail.com <kyle.smallwood@gmail.com>
2019-07-24 14:59:34 +0100

    Initial commit

---
+++ service.go
@@ -0,0 +1,37 @@
+package vault
+
+import
+(
+	"context"
+	"golang.org/x/crypto/bcrypt"
+)
+
+//a service to provide password hashing capabilities
+type Service interface{
+	Hash(ctx context.Context, password string) (string, error)
+	Validate(ctx context.Context, password, hash string) (bool, error)
+}
+
+type vaultService struct{
+
+}
+
+func NewService() Service{
+	return vaultService{}
+}
+
+func (vaultService) Hash(ctx context.Context, password string) (string, error){
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil{
+		return "", err
+	}
+	return string(hash), nil
+}
+
+func (vaultService) Validate(ctx context.Context, password, hash string) (bool, error){
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		return false, nil
+	}
+	return true, nil
+}
