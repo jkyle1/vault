dd1b1fe2c6e0df66ef14b8ab5f97b675ea2f91a4
kyle.smallwood@gmail.com <kyle.smallwood@gmail.com>
2019-07-24 21:57:01 +0100

    added hashRequest and hashResponse structures; added helper method decodeHashRequest to decode JSON body of http.Request; added the validateRequest and validateResponse structures; added a decodeValidateRequest helper function for Validate method

--- service.go
+++ service.go
@@ -1,37 +1,74 @@
 package vault
 
-import
-(
+import (
 	"context"
+	"encoding/json"
 	"golang.org/x/crypto/bcrypt"
+	"net/http"
 )
 
 //a service to provide password hashing capabilities
-type Service interface{
+type Service interface {
 	Hash(ctx context.Context, password string) (string, error)
 	Validate(ctx context.Context, password, hash string) (bool, error)
 }
 
-type vaultService struct{
-
+type vaultService struct {
 }
 
-func NewService() Service{
+func NewService() Service {
 	return vaultService{}
 }
 
-func (vaultService) Hash(ctx context.Context, password string) (string, error){
+func (vaultService) Hash(ctx context.Context, password string) (string, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	if err != nil{
+	if err != nil {
 		return "", err
 	}
 	return string(hash), nil
 }
 
-func (vaultService) Validate(ctx context.Context, password, hash string) (bool, error){
+func (vaultService) Validate(ctx context.Context, password, hash string) (bool, error) {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	if err != nil {
 		return false, nil
 	}
 	return true, nil
+}
+
+type hashRequest struct {
+	Password string `json:"password"`
+}
+
+type hashResponse struct {
+	Hash string `json:"hash"`
+	Err  string `json:"err, omitempty"`
+}
+
+func decodeHashRequest(ctx context.Context, r *http.Request) (interface{}, error) {
+	var req hashRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		return nil, err
+	}
+	return req, nil
+}
+
+type validateRequest struct {
+	Password string `json:"password"`
+	Hash     string `json:"hash"`
+}
+
+type validateResponse struct {
+	valid bool   `json:"valid"`
+	Err   string `json:"err,omitempty"`
+}
+
+func decodeValidateRequest(ctx context.Context, r *http.Request) (interface{}, error) {
+	var req validateRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		return nil, err
+	}
+	return req, nil
 }
